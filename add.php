9987e7cab40cc171aa215c6d157befa8e986bc21
Redut Anatoly <56587883+Apirol@users.noreply.github.com>
2021-03-02 12:35:52 +0700

    Added delete and update methods

--- add.php
+++ add.php
@@ -4,7 +4,7 @@
 if ($_SESSION['user_id'] == NULL) : ?>
     <p>Нужно залогиниться....</p>
 <?php else : ?>
-    <form action="create.php" method="POST">
+    <form action="add_exec.php" method="POST">
         <p>Введите заголовок:<br>
             <input type="text" name="title" />
         </p>
